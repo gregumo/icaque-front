33e97215be92f7444e0501795ccf740aeb78e429
Gregoire Humeau <gregoire@appventus.com>
2015-11-27 14:05:47 +0100

    Initial commit

---
+++ app/injector.js
@@ -0,0 +1,22 @@
+angular.module('PocApp', ['ngMaterial', 'ngRoute', 'restangular'])
+    .config(['RestangularProvider', function(RestangularProvider) {
+
+        RestangularProvider.setBaseUrl('http://api.poc.dev');
+
+        //Return only members list from getList operation
+        RestangularProvider.setResponseExtractor(function(response, operation) {
+            if (operation === 'getList') {
+                return response['hydra:member'];
+            }
+            return response;
+        });
+
+        //Remove payLoad from DELETE requests
+        RestangularProvider.setRequestInterceptor(function(elem, operation) {
+            if (operation === "remove") {
+                return null;
+            }
+            return elem;
+        });
+
+}]);
