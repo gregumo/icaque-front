e4a55d0570e0e010ff229dbc0f233c847661d404
Gregoire Humeau <gregoire@appventus.com>
2015-12-02 09:39:44 +0100

    Add LoginController

--- src/modules/user/login/LoginController.js
+++ src/modules/user/login/LoginController.js
@@ -1,3 +1,18 @@
 'use strict';
-angular.module('user').controller('LoginController', function () {
-});
+angular.module('user').controller('LoginController', ['$scope', 'Restangular', '$auth', 'toastr', '$location', function($scope, Restangular, $auth, toastr, $location) {
+    $scope.throwForm = function() {
+        Restangular.one("login_check").customPOST($scope.user).then(function(response) {
+            $scope.errors = null;
+            if(response.status == 200) {
+                console.log(response.data.token);
+                $auth.setToken(response.data.token);
+                $location.path('/');
+            }
+        })
+        .catch(function(response) {
+            if (response.data.message.length > 0) {
+                toastr.error(response.data.message);
+            }
+        });
+    }
+}]);
